47d1072766daefc07f87abbcba2398b7d32acada
Iibaalll <iqbal1901.mi@gmail.com>
2024-12-24 02:35:56 +0700

    Membuat search form

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,4 @@
-// Toggle
+// Toggle Menu
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const navbarActive = document.querySelector(".navbar-nav");
 // Ketika Hamburger menu di klik
@@ -9,5 +9,21 @@ hamburgerMenu.addEventListener("click", function () {
 document.addEventListener("click", function (e) {
   if (!hamburgerMenu.contains(e.target) && !navbarActive.contains(e.target)) {
     navbarActive.classList.remove("active");
+  }
+});
+
+// Toggle Search
+const search = document.getElementById("search");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+search.addEventListener("click", function (e) {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+});
+// Klik dimanapun kecuali search dan search-form untuk menghilangkan search form
+document.addEventListener("click", function (e) {
+  if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
