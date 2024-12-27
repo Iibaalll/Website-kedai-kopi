e8fa92f32d57b1935b67e7b4acf68b0068cc8728
Iibaalll <iqbal1901.mi@gmail.com>
2024-12-28 01:55:30 +0700

    Menyelesaikan fitur detail modal box

--- Javascript/script.js
+++ Javascript/script.js
@@ -31,8 +31,9 @@ document.addEventListener("click", function (e) {
 // Toggle untuk Shopping Cart
 const btnShop = document.getElementById("shopping-bag");
 const shoppingCart = document.querySelector(".shoping-cart");
-btnShop.addEventListener("click", function () {
+btnShop.addEventListener("click", function (e) {
   shoppingCart.classList.toggle("active");
+  e.preventDefault();
 });
 // Klik dimanapun kecuali shop dan shopping cart untuk menghilangkan shopping cart
 document.addEventListener("click", function (e) {
@@ -40,14 +41,40 @@ document.addEventListener("click", function (e) {
     shoppingCart.classList.remove("active");
   }
 });
-// Untuk menghapus Produk di shopping cart
-const removeItem = querySelectorAll(".remove-item");
-const cartItem = querySelector(".cart-item");
-removeItem.forEach(function (index) {
-  index.addEventListener("click", function () {
-    cartItem.style.display = "none";
+// Toggle Detai Modal Box
+const detailButton = document.querySelectorAll(".detail-btn");
+const modalDetail = document.querySelectorAll("#item-detail-modal");
+
+detailButton.forEach((btn, nomor) => {
+  btn.addEventListener("click", function (e) {
+    modalDetail[nomor].style.display = "flex";
+    e.preventDefault();
   });
 });
+
+// Tombol keluar dar detail Modal Box
+const exitModalbtn = document.querySelectorAll(".close-icon");
+exitModalbtn.forEach((btn, nomor) => {
+  btn.addEventListener("click", function (e) {
+    modalDetail[nomor].style.display = "none";
+    e.preventDefault();
+  });
+});
+// Bisa keluar jika diklik dimanapun kecuali modalbox
+window.addEventListener("click", function (e) {
+  if (e.target == modalDetail) {
+    modalDetail.style.display = "none";
+  }
+});
+
+// Untuk menghapus Produk di shopping cart
+// const removeItem = querySelectorAll(".remove-item");
+// const cartItem = querySelector(".cart-item");
+// removeItem.forEach(function (index) {
+//   index.addEventListener("click", function () {
+//     cartItem.style.display = "none";
+//   });
+// });
 // shoppingCart.addEventListener("click", function (e) {
 //   if (e.target.className == "remove-item") {
 //     cartItem.style.display = "none";
